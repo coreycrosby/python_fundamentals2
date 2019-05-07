c242acaba75e5de0dd944b0db01aebfd51062767
Corey Crosby <ccc_13@live.ca>
2019-05-07 17:12:11 -0400

    exercise 1-3

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+def double(my_number):
+    return (my_number) * 2
+
+print(double(3))
+print(double(5))
+print(double(7))
+
+
+
+
---
+++ exercise2.py
@@ -0,0 +1,8 @@
+def negative(num):
+    if num >= 0:
+        return False
+    if num < 0: 
+        return True
+
+print(negative(5))
+print(negative(-2))
